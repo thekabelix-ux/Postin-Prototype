2c1eebe96c01609541e571f162eabff6da7a5f23
Babirecki Mikołaj <bm57731>
2026-05-18 13:04:48 +0200

    Start

---
+++ Aplikacja/Uzytkownik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postin.Aplikacja
+{
+    abstract public class Uzytkownik
+    {
+
+    }
+
+    public class Kurier : Uzytkownik
+    {
+
+    }
+}
--- Main.cs
+++ Main.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using Postin.Aplikacja;
+
 namespace Postin
 {
     class Program
